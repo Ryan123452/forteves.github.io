592b56cf55fe2d32291e61e017944cc15ec3844e
Ryan123452 <91047765+Ryan123452@users.noreply.github.com>
2022-09-23 03:09:39 -0800

    Added New File

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    titleWeb: "Teves",
+    introTitle: 'Julia Jane Teves',
+    introDesc: `"It’s okay to feel broken right now. You will feel better soon, I promise. Stay strong."`,
+    btnIntro: '^^Proceed^^',
+    title: 'Life is too short to be sad, and I know that you’ll get through this tough time. You’re just too strong for me to worry about you. Stay strong, beautiful.You’re strong Right?',
+    btnYes: 'Yes I’am',
+    btnNo: 'No',
+    question: 'Don’t beat yourself up about it. It’s okay to be sad. Sit back and relax for a bit. aw pag Assignment sa diay haha',
+    btnReply: 'Sent',
+    reply: 'Trust yourself. You’ve survived a lot, and you’ll survive whatever is coming.',
+    mess: 'You are stronger than you think. It will be okay',
+    messDesc: 'Kung broken kapa gihapon free to chat me haha kung dika di ayaw haha suko ka? kung mangotana ka asa na nako na kuha mga pic nimo secret hahaha',
+    btnAccept: 'Continue',
+    messLink: 'https://ibb.co/styBMqH'
+}
